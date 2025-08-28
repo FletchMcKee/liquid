737241bc8248bc83ff119f44125efe6ed6fad181
FletchMcKee <cfmckee.dev@gmail.com>
2025-08-28 10:15:24 -0500

    Added maven publishing, almost ready for release

--- liquid/build.gradle.kts
+++ liquid/build.gradle.kts
@@ -1,9 +1,12 @@
 // Copyright 2025, Colin McKee
 // SPDX-License-Identifier: Apache-2.0
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+
 plugins {
   alias(libs.plugins.liquid.android.library)
   alias(libs.plugins.liquid.android.library.compose)
   alias(libs.plugins.binary.compatibility.validator)
+  alias(libs.plugins.maven.publish)
 }
 
 android {
@@ -17,4 +20,14 @@ kotlin {
 dependencies {
   api(libs.compose.ui)
   implementation(libs.compose.foundation)
+}
+
+mavenPublishing {
+  configure(
+    AndroidSingleVariantLibrary(
+      variant = "release",
+      sourcesJar = true,
+      publishJavadocJar = true,
+    ),
+  )
 }
