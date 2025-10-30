efaa0f1c78e53a1333110ea45059fae4220efe0f
FletchMcKee <cfmckee.dev@gmail.com>
2025-10-30 12:23:55 -0500

    spotlessApply

--- core/testing/src/webMain/kotlin/io/github/fletchmckee/liquid/core/testing/testUtils.web.kt
+++ core/testing/src/webMain/kotlin/io/github/fletchmckee/liquid/core/testing/testUtils.web.kt
@@ -1,3 +1,5 @@
+// Copyright 2025, Colin McKee
+// SPDX-License-Identifier: Apache-2.0
 package io.github.fletchmckee.liquid.core.testing
 
 import androidx.compose.runtime.Composable
@@ -6,5 +8,5 @@ actual abstract class ScreenshotTest
 
 actual fun runScreenshotTest(
   testName: String,
-  content: @Composable (() -> Unit)
+  content: @Composable (() -> Unit),
 ) = Unit
--- samples/composeApp/src/screenshotTest/kotlin/io/github/fletchmckee/liquid/samples/app/LiquidScreenshotTest.kt
+++
@@ -1,4 +0,0 @@
-package io.github.fletchmckee.liquid.samples.app
-
-class LiquidScreenshotTest {
-}
